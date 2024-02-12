1abde8714941d8ea68f6f3c0c6b898d9f69b5e77
Seth Patel <sethpatella@gmail.com>
2024-02-12 15:25:51 +1300

    Uploading current website
    Current kanteen website

---
+++ main.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('.toggle');
+const menu = document.querySelector('.nav');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
