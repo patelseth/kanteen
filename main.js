8a973385caf15d7889661fa1cce8e017f6fc938b
Seth Patel <sethpatella@gmail.com>
2024-03-10 19:47:11 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const toggleBtn = document.querySelector('.toggle');
-const menu = document.querySelector('.nav');
+// const toggleBtn = document.querySelector('.toggle');
+// const menu = document.querySelector('.nav');
 
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-});
+// toggleBtn.addEventListener('click', () => {
+//   menu.classList.toggle('active');
+// });
